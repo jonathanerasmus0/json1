89ec52cb671266bf7922a0d11c32b4fb86770552
Jonathan Davies <jonathan.davies@dci-student.org>
2024-02-14 09:04:12 +0100

    json

---
+++ lesson_01.py
@@ -0,0 +1,17 @@
+import json
+
+file = "01_names.json"
+
+with open(file, "r") as json_file:
+    data = json.load(json_file)
+    print(data)
+
+    
+    
+    
+    
+    
+    
+        
+    
+    
